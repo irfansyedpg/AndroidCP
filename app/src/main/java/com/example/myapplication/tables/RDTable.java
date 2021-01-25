d7d0dcf9463ccc084ca688ea6ea629b4d7708fd3
irfansyed.pg <irfansyed.pg@gmail.com>
2021-01-25 12:17:41 +0500

    Update 1/25/2021

---
+++ app/src/main/java/com/example/myapplication/tables/RDTable.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.tables;
+
+public class RDTable {
+
+    public  static  String rd1="rd1";
+    public  static  String rd2="rd2";
+    public  static  String rd3="rd3";
+    public  static  String rd5="rd5";
+    public  static  String rd6a="rd6a";  // picturea 1
+    public  static  String rd6b="rd6b";  // picturea 2
+    public  static  String rd6c="rd6c";  // picturea 3
+    public  static  String rd7="rd7";
+    public  static  String logid="logid";
+}
